31c91ecbd2ad6627d2f7a1828e6fcd29ded8517e
github username <github email address>
2020-01-26 17:46:10 -0600

    finished up to lesson 12

---
+++ ex10.py
@@ -0,0 +1,21 @@
+#tab
+tabby_cat = "\tI'm tabbed in."
+
+#newline
+persian_cat = "I'm split\non a line."
+
+#backslash
+backslash_cat = "I'm \\ a \\ cat."
+
+#""" = multi-line string
+fat_cat = """
+I'll do a list:
+\t* Cat Food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
